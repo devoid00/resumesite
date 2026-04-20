d6f77c569e48c308ddd52db5a7f879fe0023d3d9
Tommy <Tommyh.r2019@gmail.com>
2026-04-20 00:53:44 -0500

    finish about page + rails + media fixes

--- src/app/components/PorscheRail.tsx
+++ src/app/components/PorscheRail.tsx
@@ -4,15 +4,15 @@ export default function FirstCarRail() {
   return (
     <StoryRail
       items={[
-        { src: "/images/first-car/1.jpg" },
-        { src: "/images/first-car/2.jpg" },
-        { src: "/images/first-car/3.jpg" },
-        { src: "/images/first-car/4.jpg" },
+        { src: "/images/IMG_8599.jpg" },
+
         {
           type: "video",
           layout: "portrait",
-          src: "",
+          label:"Porsche Tear Down And Paint",
+          src: "https://hfog4ru2flp5mmzd.public.blob.vercel-storage.com/porscheteardown.mp4",
         },
+        { src: "/images/IMG_8618.jpg" },
       ]}
     />
   );
